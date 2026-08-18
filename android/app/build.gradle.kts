dc6187fcdc9a05593e8956facecfb2d9b1389268
BriskoBrice <120757459+BriskoBrice@users.noreply.github.com>
2026-08-18 20:33:48 +0200

    feat: bump Android release to 1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "fr.electronlibre.quizlibre"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     compileOptions {
