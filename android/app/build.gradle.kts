cbb923e05cdbd328ebf32555788381a0f6d676d2
BriskoBrice <120757459+BriskoBrice@users.noreply.github.com>
2026-08-19 11:03:17 +0200

    chore: bump Quiz Libre to V6.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "fr.electronlibre.quizlibre"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     compileOptions {
